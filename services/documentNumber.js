7fbfb7ced26619e1501954f631cabc71b23c90b6
vishal20034 <nagbishal07@gmail.com>
2026-06-13 03:23:39 +0530

    Update

--- services/documentNumber.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-// Canonical implementation lives in utils/documentNumber.js.
-// This re-export keeps existing require() paths working.
-module.exports = require("../utils/documentNumber");
